a283a93cb4416f54225c5f58e156d1b43d358f42
sandesh <sldinkar@gmail.com>
2020-09-02 23:30:05 +0530

    Addedd jobs to home

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-#new
+# new
 from django.conf import settings
 from django.conf.urls.static import static
 
+# from jobs import views
+import jobs.views
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path('', jobs.views.home, name="home"),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
